f6e7b193741ea2e688081afde979b628343b5d31
Angus Wu <anguswu123@gmail.com>
2020-11-15 19:48:03 -0800

    does not delete :'(

--- ArcticExpeditionResearcher.php
+++ ArcticExpeditionResearcher.php
@@ -179,7 +179,7 @@
 
             // Your username is ora_(CWL_ID) and the password is a(student number). For example, 
 			// ora_platypus is the username and a12345678 is the password.
-            $db_conn = OCILogon("ora_benson0", "a28598183", "dbhost.students.cs.ubc.ca:1522/stu");
+            $db_conn = OCILogon("ora_wuangus", "a37588688", "dbhost.students.cs.ubc.ca:1522/stu");
 
             if ($db_conn) {
                 debugAlertMessage("Database is Connected");
