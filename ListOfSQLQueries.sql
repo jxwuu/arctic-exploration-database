780416aafdbe61b2ed141b347bc363f45e1e23ed
bensonfan0 <benson.fan0@gmail.com>
2020-11-16 16:50:03 -0800

    added the queries I did for ListOFSQLQUERIES
    Created folders for the screenshots to make it easier to navigate

--- ListOfSQLQueries.sql
+++ ListOfSQLQueries.sql
@@ -13,11 +13,31 @@ AND $attribute3 $condition2 $number2
 -- DELETION QUERY --
 
 -- Delete lost or broken cargo --
+/*$newPlantAge is set by user to indicate new age of plant. $plantID is primary key of plant
+table which is also specified by user to determine which plant to update.*/
+UPDATE plants 
+SET age=$newPlantAge 
+WHERE plantID=$plantID
+
+-- UPDATE OPERATION --
+
+-- Update plant age --
 /*$idDelete is the cargoID specified by the user to delete
   Tuples with the cargoID is deleted in relevant tables: 
   Cargo, Food, ScientificEquipment, transportedBy, eats*/
 DELETE FROM Cargo
 WHERE CargoID = $idDelete
+
+
+-- PROJECTION QUERY --
+
+-- Project from persons --
+/*$attributeK where K is 1, 2, ... , 5 is specificed by user to indicate
+which attributes from Person table to select. The user must indicate at
+least 3 and at most 5 attributes.*/
+SELECT $attribute1, $attribute2, $attribute2, $attribute5, $attribute4
+FROM Person
+
 
 -- AGGREGATION WITH GROUP BY QUERY --
 
@@ -31,6 +51,34 @@ AND t.VehicleID = ev.VehicleID
 GROUP BY ev.VehicleType
 
 
+-- NESTED AGGREGATION WITH GROUP BY --
+
+-- Find Researcher(s) Who is Studying every Animal on Record --
+/*$groupBy, $table is specified by user. $table must be chosen first by user, which then prompts another drop down menu 
+to appear which presents $groupBy attributes which corresponds only to that current table. Finds the total count of 
+animals grouping by either diet or vertebrate. Finds the total count of plants grouping by plants with the same age.
+Finds the total count of scientific equipment grouping by if fragile.*/
+
+SELECT $groupBy, count(*)
+FROM $table
+GROUP BY $groupBy
+
+
+-- DIVISION --
+
+-- Find count of research items --
+/*Finds all researchers which are currently studying every animal in the database. Note: there is another query which
+finds the name of the Researcher given the personID afterwards, but that is separate from the division query itself*/
+
+SELECT r.PersonID 
+FROM Researcher r 
+WHERE not exists ( SELECT a.animalID
+                    FROM animal a 
+                    WHERE not exists ( SELECT s.PersonID
+                                        FROM Studies s
+                                        Where r.PersonID = s.PersonID 
+                                        AND a.AnimalID = s.AnimalID))
+
 -- EXTRA FUNCTIONS --
 
 -- Display all the tuples inside a table --
@@ -43,3 +91,7 @@ SELECT * FROM $table
 
 -- Count the number of tuples in the Person table --
 SELECT Count(*) FROM Person
+
+-- show Plant IDS / age --
+SELECT plantID, age
+FROM plants
